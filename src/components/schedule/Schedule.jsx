f9eadf1227a64b48f079d337f4500b22ea47c853
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-01-23 14:18:05 +0000

    Schedule component added

---
+++ src/components/schedule/Schedule.jsx
@@ -0,0 +1,77 @@
+import "./schedule.css";
+
+const Schedule = () => {
+  return (
+    <div className="schedule">
+      <h1 className="scheduleTitle">Schedule</h1>
+      <div className="scheduleContainer">
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">20</h1>
+            <h1 className="scheduleContentDateCont">JAN</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">
+              Last date to register
+            </h3>
+            <p className="scheduleContentDetailsText">
+              All the *team leaders* are expected to register their team before
+              the mentioned deadline. After the deadline, applications will be
+              no longer processed.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">20</h1>
+            <h1 className="scheduleContentDateCont">JAN</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">
+              Last date to register
+            </h3>
+            <p className="scheduleContentDetailsText">
+              All the *team leaders* are expected to register their team before
+              the mentioned deadline. After the deadline, applications will be
+              no longer processed.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">20</h1>
+            <h1 className="scheduleContentDateCont">JAN</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">
+              Last date to register
+            </h3>
+            <p className="scheduleContentDetailsText">
+              All the *team leaders* are expected to register their team before
+              the mentioned deadline. After the deadline, applications will be
+              no longer processed.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">20</h1>
+            <h1 className="scheduleContentDateCont">JAN</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">
+              Last date to register
+            </h3>
+            <p className="scheduleContentDetailsDesc">
+              All the *team leaders* are expected to register their team before
+              the mentioned deadline. After the deadline, applications will be
+              no longer processed.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Schedule;
