5ad45e24a5d4a55a3d42f5d132f47f788f75e984
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-01-24 17:11:53 +0000

    ContactUs Component Added By Suyash

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Aboutevent from "./components/aboutevent/Aboutevent";
 import Schedule from "./components/schedule/Schedule";
 import Prizes from "./components/prizes/Prizes";
 import Eventteam from "./components/eventteam/Eventteam";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
       <Sponsors />
       <Eventteam />
       <Faques />
+      <Contact />
     </>
   );
 };
