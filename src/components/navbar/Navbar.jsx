0ca754ca58680db7d8262115ddeb43c4e5a6e812
Avishkar Dhanorkar <136093003+avidhanorkar@users.noreply.github.com>
2024-01-26 17:32:36 +0000

    By avishkar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,36 +1,49 @@
 import "./navbar.css";
 const Navbar = () => {
+  const showSidebar = () => {
+    document.querySelector(".sidebar").style.display = 'flex';
+  }
+  const closeSidebar = () =>{
+    document.querySelector(".sidebar").style.display = "none";
+  }
   return (
     <div className="navbar">
       <div className="container">
         <div className="logoContainer">
           <img className="logoImg" src="assets/Devkrafttrans.png" alt="" />
         </div>
-        <div className="nav-elements">
+        <nav className="nav-elements">
+          <ul className="sidebar">
+<svg onClick = {closeSidebar} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+            <li></li>
+            <li>About</li>
+            <li>Schedule</li>
+            <li>Prizes</li>
+            <li>Sponsors</li>
+            <li>FAQ</li>
+            <li>EventTeam</li>
+            <li>Contact</li>
+          </ul>
           <ul className="navList">
-            <li>
-              <span className="navLinks">About</span>
-            </li>
-            <li>
-              <span className="navLinks">Schedule</span>
-            </li>
-            <li>
-              <span className="navLinks">Prizes</span>
-            </li>
-            <li>
-              <span className="navLinks">Sponsors</span>
-            </li>
-            <li>
-              <span className="navLinks">FAQ</span>
-            </li>
-            <li>
-              <span className="navLinks">EventTeam</span>
-            </li>
-            <li>
-              <span className="navLinks">Contact</span>
+            <li className="navLinks">About</li>
+            <li className="navLinks">About</li>
+            <li className="navLinks">About</li>
+            <li className="navLinks">About</li>
+            <li className="navLinks">About</li>
+            <li className="navLinks">About</li>
+            <li className="menu-btn">
+              <svg onClick={showSidebar()}
+                xmlns="http://www.w3.org/2000/svg"
+                height="30"
+                viewBox="0 -960 960 960"
+                width="30"
+                fill="rgb(255, 255, 255)"
+              >
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+              </svg>
             </li>
           </ul>
-        </div>
+        </nav>
       </div>
     </div>
   );
