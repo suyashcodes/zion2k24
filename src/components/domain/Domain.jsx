cb015422d4b8a432be4250dfbcf4e6c217934f57
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-01-27 09:19:45 +0000

    Responsivess updates By Suyash

--- src/components/domain/Domain.jsx
+++ src/components/domain/Domain.jsx
@@ -9,66 +9,36 @@ const Domain = () => {
         </div>
         <div className="trackBoxes">
           <div className="trackBox">
-            <h5 className="trackHeading">AIML</h5>
+            <h5 className="trackHeading">WEB DEV</h5>
             <p className="trackDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-              reprehenderit optio amet ab temporibus asperiores quasi
-              cupiditate. Voluptatum ducimus voluptates voluptas?
+              For the Web Development enthusiasts, this hackathon is your canvas
+              to paint the digital world with your coding prowess. Dive into
+              front-end frameworks, back-end technologies, and create seamless,
+              user-friendly interfaces. Build websites and applications that not
+              only function flawlessly but also push the boundaries of what's
+              possible in the digital space.
             </p>
           </div>
           <div className="trackBox">
             <h5 className="trackHeading">AIML</h5>
             <p className="trackDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-              reprehenderit optio amet ab temporibus asperiores quasi
-              cupiditate. Voluptatum ducimus voluptates voluptas?
+              In the AIML domain, sharpen your skills in crafting intelligent
+              algorithms and systems that mimic human-like learning. From
+              natural language processing to computer vision, the possibilities
+              are limitless. Seize this opportunity to explore the cutting-edge
+              advancements in AI and leave your mark on the landscape of
+              intelligent technologies.
             </p>
           </div>
           <div className="trackBox">
-            <h5 className="trackHeading">AIML</h5>
+            <h5 className="trackHeading">WEB 3.0</h5>
             <p className="trackDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-              reprehenderit optio amet ab temporibus asperiores quasi
-              cupiditate. Voluptatum ducimus voluptates voluptas?
-            </p>
-          </div>
-          <div className="trackBox">
-            <h5 className="trackHeading">AIML</h5>
-            <p className="trackDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-              reprehenderit optio amet ab temporibus asperiores quasi
-              cupiditate. Voluptatum ducimus voluptates voluptas?
-            </p>
-          </div>
-          <div className="trackBox">
-            <h5 className="trackHeading">AIML</h5>
-            <p className="trackDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-              reprehenderit optio amet ab temporibus asperiores quasi
-              cupiditate. Voluptatum ducimus voluptates voluptas?
-            </p>
-          </div>
-          <div className="trackBox">
-            <h5 className="trackHeading">AIML</h5>
-            <p className="trackDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-              reprehenderit optio amet ab temporibus asperiores quasi
-              cupiditate. Voluptatum ducimus voluptates voluptas?
-            </p>
-          </div>
-          <div className="trackBox">
-            <h5 className="trackHeading">AIML</h5>
-            <p className="trackDesc">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-              non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-              reprehenderit optio amet ab temporibus asperiores quasi
-              cupiditate. Voluptatum ducimus voluptates voluptas?
+              Now, brace yourselves for the frontier of technology with Web 3.0.
+              The next evolution of the internet is here, promising
+              decentralized, user-centric experiences. Explore blockchain, smart
+              contracts, and decentralized applications (DApps) to revolutionize
+              the way we interact online. This is your chance to be at the
+              forefront of the internet's transformation.
             </p>
           </div>
         </div>
