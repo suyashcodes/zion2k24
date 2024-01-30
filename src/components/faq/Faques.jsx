bc01ba70c21d15a648224aa6b78c698a62112218
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-01-30 12:05:40 +0000

    Event team, FAQ section updated

--- src/components/faq/Faques.jsx
+++ src/components/faq/Faques.jsx
@@ -7,37 +7,37 @@ const FAQSection = () => {
     {
       question: "What if I don't know how to code?",
       answer:
-        "No worries, Interest in learning and working with technology is much more important than your current experience level.",
+        "No worries, interest in learning and working with technology is much more important than your current experience level.",
     },
     {
       question: "How much will it cost?",
       answer:
-        "Registration for DEVCLASH is completely free, Accomodation and food is completely free for shortlisted teams as well.",
+        "Registration for DEVCLASH is completely free, but we are charging only 200Rs for hospitality and meals.",
     },
     {
-      question: "What if I don't know how to code?",
-      answer:
-        "No worries, Interest in learning and working with technology is much more important than your current experience level.",
+      question: "How many members are allowed in a team?",
+      answer: "Minimum 1 and maximum of 4 members per team are allowed.",
     },
     {
-      question: "What if I don't know how to code?",
+      question: "How long does the review process take?",
       answer:
-        "No worries, Interest in learning and working with technology is much more important than your current experience level.",
+        "The review process can take several days to weeks, depending on the complexity of the project and the number of applications received. We will do our best to keep you informed throughout the process and provide feedback on your application.",
     },
     {
-      question: "What if I don't know how to code?",
+      question: "What resources will be available during the hackathon?",
       answer:
-        "No worries, Interest in learning and working with technology is much more important than your current experience level.",
+        "High-Speed Wi-Fi: We provide high-speed Wi-Fi access to ensure teams have a seamless and uninterrupted internet connection throughout the hackathon. College Computer Labs: Our college computer labs are equipped with the latest hardware and software necessary for project building. Teams are welcome to utilize these facilities for coding and development. Personal Hardware: Teams have the flexibility to use their personal hardware devices (laptops, computers, etc.) during the hackathon. Our local area network (LAN) will be configured to allow seamless connectivity to the college Wi-Fi. Damage Recovery Policy: In the rare event of damage to college-provided hardware, we have implemented a damage recovery policy. If any hardware is accidentally damaged during the hackathon, the entire team will be responsible for covering the costs of the damage. This policy encourages teams to handle equipment with care and fosters a sense of responsibility among participants.",
     },
     {
-      question: "What if I don't know how to code?",
+      question: "How can I contact the organizers if I have further questions?",
       answer:
-        "No worries, Interest in learning and working with technology is much more important than your current experience level.",
+        "Have queries? Connect with us! Reach out to our team members, drop an email at clubname@example.com, or slide into our DMs on LinkedIn (Club Name). Check our website's Contact section for more ways to reach us. We're here to assist you!",
     },
     {
-      question: "Why do we use it?",
+      question:
+        "Is there a code of conduct that participants need to adhere to?",
       answer:
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+        "Yes, participants are expected to adhere to our Code of Conduct throughout the event. Please familiarize yourself with the guidelines outlined https://devfolio.co/code-of-conduct. We believe in fostering a respectful and inclusive environment for everyone involved.",
     },
     // Add more dummy questions and answers as needed
   ];
--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -7,65 +7,123 @@ const Schedule = () => {
       <div className="scheduleContainer">
         <div className="scheduleContent">
           <div className="scheduleContentDate">
-            <h1 className="scheduleContentDateCont">20</h1>
+            <h1 className="scheduleContentDateCont">29</h1>
             <h1 className="scheduleContentDateCont">JAN</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">Registration opens</h3>
+            <p className="scheduleContentDetailsText">
+              On this day, the registration portal will open for team leaders to
+              enroll themselves and their respective teams. Teams should ensure
+              timely registration to secure their spot in the hackathon.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">5</h1>
+            <h1 className="scheduleContentDateCont">FEB</h1>
           </div>
           <div className="scheduleContentDetails">
             <h3 className="scheduleContentDetailsTitle">
               Last date to register
             </h3>
             <p className="scheduleContentDetailsText">
-              All the *team leaders* are expected to register their team before
-              the mentioned deadline. After the deadline, applications will be
-              no longer processed.
+              This marks the deadline for all teams and individual participants
+              to complete their registration process. Late registrations will
+              not be considered, so make sure to register before this date.
             </p>
           </div>
         </div>
         <div className="scheduleContent">
           <div className="scheduleContentDate">
-            <h1 className="scheduleContentDateCont">20</h1>
-            <h1 className="scheduleContentDateCont">JAN</h1>
+            <h1 className="scheduleContentDateCont">6</h1>
+            <h1 className="scheduleContentDateCont">FEB</h1>
           </div>
           <div className="scheduleContentDetails">
             <h3 className="scheduleContentDetailsTitle">
-              Last date to register
+              Shortlisting Announcement
             </h3>
             <p className="scheduleContentDetailsText">
-              All the *team leaders* are expected to register their team before
-              the mentioned deadline. After the deadline, applications will be
-              no longer processed.
+              Candidates who successfully registered will be informed about the
+              shortlisting of teams and individuals. This announcement sets the
+              stage for the next steps in the hackathon journey.
             </p>
           </div>
         </div>
         <div className="scheduleContent">
           <div className="scheduleContentDate">
-            <h1 className="scheduleContentDateCont">20</h1>
-            <h1 className="scheduleContentDateCont">JAN</h1>
+            <h1 className="scheduleContentDateCont">7</h1>
+            <h1 className="scheduleContentDateCont">FEB</h1>
           </div>
           <div className="scheduleContentDetails">
             <h3 className="scheduleContentDetailsTitle">
-              Last date to register
-            </h3>
-            <p className="scheduleContentDetailsText">
-              All the *team leaders* are expected to register their team before
-              the mentioned deadline. After the deadline, applications will be
-              no longer processed.
-            </p>
-          </div>
-        </div>
-        <div className="scheduleContent">
-          <div className="scheduleContentDate">
-            <h1 className="scheduleContentDateCont">20</h1>
-            <h1 className="scheduleContentDateCont">JAN</h1>
-          </div>
-          <div className="scheduleContentDetails">
-            <h3 className="scheduleContentDetailsTitle">
-              Last date to register
+              Hospitality Charges Payment Deadline
             </h3>
             <p className="scheduleContentDetailsDesc">
-              All the *team leaders* are expected to register their team before
-              the mentioned deadline. After the deadline, applications will be
-              no longer processed.
+              Shortlisted teams are required to pay a hospitality fee of INR 200
+              per team. Payment must be completed before January 8, 2024, to
+              proceed to the next phase.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">8</h1>
+            <h1 className="scheduleContentDateCont">FEB</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">
+              Confirmation of Shortlisting
+            </h3>
+            <p className="scheduleContentDetailsDesc">
+              Following the verification of payments, all shortlisted teams will
+              receive a confirmation email. This confirmation is crucial for
+              teams to officially participate in the hackathon.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">10</h1>
+            <h1 className="scheduleContentDateCont">FEB</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">Hackathon Kickoff</h3>
+            <p className="scheduleContentDetailsDesc">
+              The official commencement of the hackathon! Participants can start
+              working on their projects. Resources, guidelines, and support will
+              be provided to ensure a smooth start to the coding journey.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">11</h1>
+            <h1 className="scheduleContentDateCont">FEB</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">Hackathon Ends</h3>
+            <p className="scheduleContentDetailsDesc">
+              Participants need to wrap up their projects as the hackathon
+              concludes. Submission guidelines will be provided, and teams must
+              adhere to the deadlines for project submission.
+            </p>
+          </div>
+        </div>
+        <div className="scheduleContent">
+          <div className="scheduleContentDate">
+            <h1 className="scheduleContentDateCont">18</h1>
+            <h1 className="scheduleContentDateCont">FEB</h1>
+          </div>
+          <div className="scheduleContentDetails">
+            <h3 className="scheduleContentDetailsTitle">
+              Winners Announcement and Prize Distribution
+            </h3>
+            <p className="scheduleContentDetailsDesc">
+              The moment of truth! Winners will be announced, and prizes will be
+              distributed. It's important to note that this date is subject to
+              change based on project complexity. Stay tuned for updates.
             </p>
           </div>
         </div>
