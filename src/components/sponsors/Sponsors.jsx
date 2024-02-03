044e22bd82b7ff483ec88fdda00239f7f07fda4f
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-02-04 03:21:22 +0530

    Update Sponsors.jsx

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -7,12 +7,14 @@ const Sponsors = () => {
         <div className="domainHeading">SPONSORS</div>
       </div>
       <div className="titleSponsors">
-{/*         <img className="logo" src="assets/actualone.svg" alt="" /> */}
-        <img className="logo" src="assets/gfg.png" alt="" />
         <img className="logo aptos" src="assets/aptoslogo.jpg" alt="" />
+        <img className="logo actualone" src="assets/actualone1.jpg" alt="" />
+        <img className="logo" src="assets/gfg.png" alt="" />
         <img className="logo" src="assets/plutope.jpeg" alt="" />
         {/* <img className="logo polygon" src="assets/polygonlogo.png" alt="" /> */}
         <img className="logo polygon" src="assets/acampusai.png" alt="" />
+        {/* <img className="logo polygon" src="assets/campustimes.png" alt="" />
+        <img className="logo polygon" src="assets/punekarnews.png" alt="" /> */}
       </div>
     </div>
   );
