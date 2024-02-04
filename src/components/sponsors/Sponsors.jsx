578f1977ba1e3cf7eea4e3f20b1af4859d13784a
Suyash Dashputre <158780412+ctrlpluscsuyashcodes@users.noreply.github.com>
2024-02-04 10:36:25 +0000

    Sponsors

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -7,19 +7,15 @@ const Sponsors = () => {
         <div className="domainHeading">SPONSORS</div>
       </div>
       <div className="titleSponsors">
-        <img className="logo aptos" src="assets/aptoslogo.jpg" alt="" />
-        <img className="logo actualone" src="assets/actualone1.jpg" alt="" />
-        <img className="logo rr" src="assets/gfg2.jpg" alt="" />
-        <img className="logo" src="assets/plutope.jpeg" alt="" />
-        <img className="logo solana" src="assets/solanafinal.png" alt="" />
-        <img className="logo rr polygon" src="assets/acampusai.png" alt="" />
-        <img className="logo pun" src="assets/punekarnewsfinal.png" alt="" />
-        <img
-          className="logo polygon"
-          src="assets/campustimesfinal.png"
-          alt=""
-        />
-        <img className="logo rr" src="assets/devfolio.png" alt="" />
+        <img className="logo aptos" src="assets/aptos2hd.png" alt="" />
+        <img className="logo actualone" src="assets/actualoneff.jpg" alt="" />
+        <img className="logo rr" src="assets/gfg3.jpg" alt="" />
+        <img className="logo" src="assets/pultopehd.png" alt="" />
+        <img className="logo solana" src="assets/solanahd.png" alt="" />
+        <img className="logo rr polygon" src="assets/acampusaihd.png" alt="" />
+        <img className="logo pun" src="assets/punekarnewshd.png" alt="" />
+        <img className="logo polygon" src="assets/campustimeshd.png" alt="" />
+        <img className="logo rr" src="assets/devfoliohd.png" alt="" />
       </div>
     </div>
   );
