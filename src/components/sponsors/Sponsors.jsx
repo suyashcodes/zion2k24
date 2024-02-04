7d7acca5330d59e7e781ebee4fb5459ef12ca2ff
Suyash Dashputre <158780412+ctrlpluscsuyashcodes@users.noreply.github.com>
2024-02-04 08:10:34 +0000

    Sponsosrs

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -9,12 +9,13 @@ const Sponsors = () => {
       <div className="titleSponsors">
         <img className="logo aptos" src="assets/aptoslogo.jpg" alt="" />
         <img className="logo actualone" src="assets/actualone1.jpg" alt="" />
-        <img className="logo" src="assets/gfg.png" alt="" />
+        <img className="logo rr" src="assets/gfg2.jpg" alt="" />
         <img className="logo" src="assets/plutope.jpeg" alt="" />
-        {/* <img className="logo polygon" src="assets/polygonlogo.png" alt="" /> */}
-        <img className="logo polygon" src="assets/acampusai.png" alt="" />
-        {/* <img className="logo polygon" src="assets/campustimes.png" alt="" />
-        <img className="logo polygon" src="assets/punekarnews.png" alt="" /> */}
+        <img className="logo solana" src="assets/solanaa.png" alt="" />
+        <img className="logo rr polygon" src="assets/acampusai.png" alt="" />
+        <img className="logo pun" src="assets/punekarnews3.png" alt="" />
+        <img className="logo rr polygon" src="assets/campustimess.png" alt="" />
+        <img className="logo hid" src="assets/campustimess.png" alt="" />
       </div>
     </div>
   );
