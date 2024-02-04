b73b81b1bfab958e31528ad33f61c0b8f91b4d52
Suyash Dashputre <158780412+ctrlpluscsuyashcodes@users.noreply.github.com>
2024-02-04 09:54:52 +0000

    Sponsors updated

--- src/components/sponsors/Sponsors.jsx
+++ src/components/sponsors/Sponsors.jsx
@@ -11,11 +11,15 @@ const Sponsors = () => {
         <img className="logo actualone" src="assets/actualone1.jpg" alt="" />
         <img className="logo rr" src="assets/gfg2.jpg" alt="" />
         <img className="logo" src="assets/plutope.jpeg" alt="" />
-        <img className="logo solana" src="assets/solanaa.png" alt="" />
+        <img className="logo solana" src="assets/solanafinal.png" alt="" />
         <img className="logo rr polygon" src="assets/acampusai.png" alt="" />
-        <img className="logo pun" src="assets/punekarnews3.png" alt="" />
-        <img className="logo rr polygon" src="assets/campustimess.png" alt="" />
-        <img className="logo hid" src="assets/campustimess.png" alt="" />
+        <img className="logo pun" src="assets/punekarnewsfinal.png" alt="" />
+        <img
+          className="logo polygon"
+          src="assets/campustimesfinal.png"
+          alt=""
+        />
+        <img className="logo rr" src="assets/devfolio.png" alt="" />
       </div>
     </div>
   );
