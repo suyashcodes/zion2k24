6f636e4fec89811d51dd2b31b72b26ecac147c32
Avishkar Dhanorkar <avishkardhanorkar426@gmail.com>
2024-03-12 19:08:12 +0000

    everything updated

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -101,10 +101,10 @@ const Timer = () => {
       </div>
       <div class="wrapper">
         <div class="box">
-          Explore Events
+          <a href="#domain">
+            <p>Explore Events</p>
+          </a>
         </div>
-
-
       </div>
     </div>
   );
