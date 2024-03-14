bdd26a9e04e09f8b6a9c932200e654e9ca30844f
Suyash Dashputre <82707695+suyashcodes@users.noreply.github.com>
2024-03-14 12:11:27 +0000

    Eventteam Updated

--- src/components/eventteam/Eventteam.jsx
+++ src/components/eventteam/Eventteam.jsx
@@ -1,5 +1,6 @@
 import "./eventteam.css";
 import { BsWhatsapp, BsLinkedin } from "react-icons/bs";
+import { AiFillMail, AiFillLinkedin } from "react-icons/ai";
 import { Fade } from "react-awesome-reveal";
 const EventTeam = () => {
   return (
@@ -15,7 +16,7 @@ const EventTeam = () => {
               alt="Team Member 1"
             />
             <h2 className="eventteam-name">Pratik Kakade</h2>
-            <p className="eventteam-position">Event Head</p>
+            {/* <p className="eventteam-position">Event Head</p> */}
             <div className="eventteam-social">
               <a
                 className="whatsapp-anchor"
@@ -23,95 +24,19 @@ const EventTeam = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <BsLinkedin className="socialImg" />
+                <AiFillMail className="socialImg" />
+              </a>
+              <a
+                className="whatsapp-anchor"
+                href="https://www.linkedin.com/in/codersuyash/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillLinkedin className="socialImg" />
               </a>
             </div>
         </div>
           {/* </Fade> */}
-        <div className="eventteam-card">
-          <Fade direction="top" duration={2000}>
-            <img
-              className="eventteam-img"
-              src="./assets/personicon.png"
-              alt="Team Member 1"
-            />
-            <h2 className="eventteam-name">Atharva Athanikar</h2>
-            <p className="eventteam-position">Event Head</p>
-            <div className="eventteam-social">
-              <a
-                className="whatsapp-anchor"
-                href="https://www.linkedin.com/in/codersuyash/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <BsLinkedin className="socialImg" />
-              </a>
-            </div>
-          </Fade>
-        </div>
-        <div className="eventteam-card">
-          <Fade direction="top" duration={2000}>
-            <img
-              className="eventteam-img"
-              src="./assets/Suyash4.png"
-              alt="Team Member 1"
-            />
-            <h2 className="eventteam-name">Suyash Dashputre</h2>
-            <p className="eventteam-position">Technical Head</p>
-            <div className="eventteam-social">
-              <a
-                className="whatsapp-anchor"
-                href="https://www.linkedin.com/in/codersuyash/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <BsLinkedin className="socialImg" />
-              </a>
-            </div>
-          </Fade>
-        </div>
-        <div className="eventteam-card">
-          <Fade direction="top" duration={2000}>
-            <img
-              className="eventteam-img"
-              src="./assets/personicon.png"
-              alt="Team Member 1"
-            />
-            <h2 className="eventteam-name">Avishkar Dhanorkar</h2>
-            <p className="eventteam-position">Technical Head</p>
-            <div className="eventteam-social">
-              <a
-                className="whatsapp-anchor"
-                href="https://www.linkedin.com/in/avidhanorkar23/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <BsLinkedin className="socialImg" />
-              </a>
-            </div>
-          </Fade>
-        </div>
-        <div className="eventteam-card">
-          <Fade direction="top" duration={2000}>
-            <img
-              className="eventteam-img"
-              src="./assets/personicon.png"
-              alt="Team Member 1"
-            />
-            <h2 className="eventteam-name">Paras Wadkar</h2>
-            <p className="eventteam-position">Technical Head</p>
-            <div className="eventteam-social">
-              <a
-                className="whatsapp-anchor"
-                href="https://www.linkedin.com/in/codersuyash/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <BsLinkedin className="socialImg" />
-              </a>
-            </div>
-          </Fade>
-        </div>
       </div>
     </div>
   );
